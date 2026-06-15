7c5160bf94cd2c7ffcfc148ba41815ea92a90e46
cogokevandra <cogokevandra@gmail.com>
2026-06-15 22:27:49 +0800

    ni sy coba tes animasi gerakan

---
+++ Animasi bolanya.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <chrono>
+using namespace std;
+
+void gerakanbola(int x, int y) {                     //1. y : nomor baris         2. H = kek pindahin kursor ke sini
+    cout << "\033[" << y << ";" << x << "H";    //3. x : nomor kolom         4. ; = pemisah antara baris dan kolom
+}//nanti jadi \033[y;xH ,jadi kek perintah pindahin kursor ke baris y kolom x
+
+void hapus() {
+    cout << "\033[2J";
+}//buat hapus layar
+
+void hidecursor() { cout << "\033[?25l"; } //biar kalo dijalanin ga ada kursornya 
+void showcursor() { cout << "\033[?25h"; } //buat nampilin kursornya lagi
+
+void sleep_ms(int ms) {
+    auto start = chrono::steady_clock::now(); 
+    //1. auto itu type data yg otomatis disesuaikan sama compiler
+    //2. chrono::steady_clock::now(); buat ambil waktu sekarang yaitu sejak komputermu pertama kali dinyalakan udah berapa detik gitu
+    
+    while (chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start).count() < ms);
+    //1. hitung selisih waktu sekarang sama yg awal, terus diubah jadi milisecond
+    //2. pake count buat ngambil hasil abis diubah jadi milisecond, karna kan hasil dari diubahnya itu bukan angka biasa jadi gabisa dibandingin sama ms yg meruakan int
+    //3. <ms buat ngecek apkh belum sampe durasi yg diminta (80)
+    //4. pake while ini biar dia terus ngecek selama 80ms buat lanjut ke gerakan bola selanjutnya ngerti ga
+}
+
+int main(){
+    const int WIDTH = 70;
+    const int Y = 10;
+
+    int ulang=0;
+
+    hidecursor();
+    hapus();
+
+   
+    for (int x = 1; x <= WIDTH; x++) {
+        hapus();
+
+        gerakanbola(x, Y);
+        cout << "O";
+
+        cout.flush(); //buat animasinya ga patah patah
+
+        sleep_ms(70);
+    }
+    
+    showcursor();
+
+    cout << "\nSelesai!\n";
+
+    system("pause");
+    return 0;
+    //ngerti ga
+}
